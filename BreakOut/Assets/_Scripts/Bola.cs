c52238f63e99c19ee8efcefa6133f113efd0d2d4
oscarifh <oscarifuenteshernandez@gmail.com>
2025-04-17 20:14:30 -0600

    Practica-Modulo-12
    Se modificaron los scripts para la bola y el jugador asi como el input para el control el cual funciono sin problema

--- BreakOut/Assets/_Scripts/Bola.cs
+++ BreakOut/Assets/_Scripts/Bola.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Bola : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] public float VelBola = 15.0f;
+    private bool isGameStarted = false;
     void Start()
     {
-        
+        Vector3 posicionInicialJugador = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicialJugador.y += 3;
+        this.transform.position = posicionInicialJugador;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space)||Input.GetButton("Submit"))
+        {
+            if (!isGameStarted)
+            {
+                isGameStarted = true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = VelBola * Vector3.up;
+            }
+
+
+        }
     }
 }
